cb882035fc2971bf1184d6fd301ca96e13412a5d
hanbyul03 <hanbyul0308@naver.com>
2025-12-05 15:48:01 +0900

    1111

---
+++ src/main/java/com/sist/web/mapper/MusicMapper.java
@@ -0,0 +1,10 @@
+package com.sist.web.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+@Mapper
+@Repository
+public interface MusicMapper {
+	
+}
---
+++ src/main/java/com/sist/web/service/MusicServiceImpl.java
@@ -0,0 +1,25 @@
+package com.sist.web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.*;
+import com.sist.web.dao.*;
+import com.sist.web.vo.*;
+@Service
+public class MusicServiceImpl implements MusicService {
+	@Autowired
+	private MusicDAO mDao;
+
+	@Override
+	public List<MusicVO> musicListData(Map map) {
+		// TODO Auto-generated method stub
+		return mDao.musicListData(map);
+	}
+
+	@Override
+	public int musicTotalPage() {
+		// TODO Auto-generated method stub
+		return mDao.musicTotalPage();
+	}
+
+}
