03d360ec3ed8cb80343376449f7e3e3f983e2dc3
hanbyul03 <hanbyul0308@naver.com>
2025-12-05 17:11:29 +0900

    2222

--- src/main/java/com/sist/web/mapper/MusicMapper.java
+++ src/main/java/com/sist/web/mapper/MusicMapper.java
@@ -3,8 +3,10 @@ package com.sist.web.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 import java.util.*;
+import com.sist.web.vo.*;
 @Mapper
 @Repository
 public interface MusicMapper {
-	
+	public List<MusicVO> musicListData(Map map);
+	public int musicTotalPage();
 }
--- src/main/java/com/sist/web/service/MusicServiceImpl.java
+++ src/main/java/com/sist/web/service/MusicServiceImpl.java
@@ -3,23 +3,22 @@ package com.sist.web.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.*;
-import com.sist.web.dao.*;
+import com.sist.web.mapper.*;
 import com.sist.web.vo.*;
 @Service
-public class MusicServiceImpl implements MusicService {
-	@Autowired
-	private MusicDAO mDao;
+public class MusicServiceImpl implements MusicService{
+   @Autowired
+   private MusicMapper mapper;
 
-	@Override
-	public List<MusicVO> musicListData(Map map) {
-		// TODO Auto-generated method stub
-		return mDao.musicListData(map);
-	}
+   @Override
+   public List<MusicVO> musicListData(Map map) {
+	// TODO Auto-generated method stub
+	return mapper.musicListData(map);
+   }
 
-	@Override
-	public int musicTotalPage() {
-		// TODO Auto-generated method stub
-		return mDao.musicTotalPage();
-	}
-
+   @Override
+   public int musicTotalPage() {
+	// TODO Auto-generated method stub
+	return mapper.musicTotalPage();
+   }
 }
